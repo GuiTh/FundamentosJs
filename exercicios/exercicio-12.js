c8301d1d6741b86adbbbf4e2b510f701fc27e3cc
GuiTh <guilhermetcarlos@hotmail.com>
2022-04-20 17:58:34 -0300

    menu do trampo pra terminar

---
+++ exercicios/exercicio-12.js
@@ -0,0 +1,10 @@
+/*12) Faça um algoritmo que calcule o fatorial de um número*/
+
+function numeroFatorial(numeroFatorial){
+    for(let i = 1;i<=numeroFatorial;i++){
+        numeroFatorado = numeroFatorial * i
+        console.log(numeroFatorado)
+    }
+}
+
+numeroFatorial(10)
--- exercicios/valorCorrigido.js
+++
@@ -1,5 +0,0 @@
-const valorCorrigido = function(real){
-    real = `R$ ${real.toFixed(2).toString().replace('.',',')}`
-    console.log(real)
-}
-valorCorrigido(0.1 + 0.2)
